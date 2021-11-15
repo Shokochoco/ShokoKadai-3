a0422e25e8b8a386e4c363ed8a69c599dba81ba4
Shokochoco <shsuzu1992@gmail.com>
2021-11-15 21:36:51 +0900

    After review

--- Kadai-3/ViewController.swift
+++ Kadai-3/ViewController.swift
@@ -1,31 +1,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet private weak var textField1: UITextField!
-    @IBOutlet private weak var textField2: UITextField!
-    @IBOutlet private weak var textLabel1: UILabel!
-    @IBOutlet private weak var textLabel2: UILabel!
-    @IBOutlet private weak var textLabel3: UILabel!
+    @IBOutlet private weak var number1TextField1: UITextField!
+    @IBOutlet private weak var number2TextField2: UITextField!
+    @IBOutlet private weak var signedNumber1Label: UILabel!
+    @IBOutlet private weak var signedNumber2Label: UILabel!
+    @IBOutlet private weak var resultLabel: UILabel!
 
     @IBOutlet private weak var switch1: UISwitch!
     @IBOutlet private weak var switch2: UISwitch!
 
     @IBAction private func button(_ sender: Any) {
-        var num1 = Int(textField1.text ?? "") ?? 0
-        var num2 = Int(textField2.text ?? "") ?? 0
+        let num1 = Int(number1TextField1.text ?? "") ?? 0
+        let num2 = Int(number2TextField2.text ?? "") ?? 0
+
+        let signedNumber1: Int // 定数用意
         if switch1.isOn { // マイナスの時
-            num1 *= -1
-            textLabel1.text = String(num1)
+            signedNumber1 = -num1
         } else {
-            textLabel1.text = String(num1)
+            signedNumber1 = num1
         }
 
+        let signedNumber2: Int
         if switch2.isOn {
-            num2 *= -1
-            textLabel2.text = String(num2)
+            signedNumber2 = -num2
         } else {
-            textLabel2.text = String(num2)
+            signedNumber2 = num2
         }
-        textLabel3.text = String(num1 + num2)
+        signedNumber1Label.text = String(signedNumber1)
+        signedNumber2Label.text = String(signedNumber2)
+        resultLabel.text = String(signedNumber1 + signedNumber2)
     }
 }
